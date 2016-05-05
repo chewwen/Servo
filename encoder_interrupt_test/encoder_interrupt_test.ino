9b2e7009456afd5c905d498a326f0595111164c2
bentanlw <bentlw@gmail.com>
2016-05-05 09:51:13 +0800

    Ramp code(trial_motor) and finding home position using encoder (encoder_interrupt_test)

---
+++ encoder_interrupt_test/encoder_interrupt_test.ino
@@ -0,0 +1,51 @@
+#include <AFMotor.h>
+#include <Encoder.h>
+
+AF_DCMotor motor(1, MOTOR12_64KHZ); //registers motor pin and sets the pwm freq
+volatile long enc_count = 0;
+int dir = BACKWARD;
+int pos = 12;
+int new_pos = 5000;
+int i = 0;
+Encoder myEnc(2, 14);
+
+void setup() {
+  // all your normal setup code
+  Serial.begin(9600);
+  home_motor(dir);
+}
+
+void home_motor(int dir) {
+  motor.setSpeed(200);
+  //motor.run(FORWARD);
+  //delay(1000);
+  while (new_pos != pos) {
+    motor.run(dir);
+    
+    i++;
+    if (i % 10 == 0) {
+      pos = new_pos;
+      new_pos = myEnc.read();
+    }
+
+    Serial.print("i: ");
+    Serial.println(i);
+    Serial.print("POS: ");
+    Serial.println(pos);
+    Serial.print("nPOS: ");
+    Serial.println(new_pos);
+  }
+  //pos = myEnc.read();
+
+  motor.run(RELEASE);
+  Serial.println("HOME!!");
+  myEnc.write(0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //home_motor();
+  //Serial.println(myEnc.read());
+
+
+}
---
+++ trial_motor/trial_motor.ino
@@ -0,0 +1,136 @@
+ /* Increase/Decrease Motor speed with S-shaped velocity profile
+  * 
+  * Sketch by Benedict Tan
+  * v. 2 -- 18042016
+  *
+  */  
+
+#include <elapsedMillis.h>  //independent timer
+#include <AFMotor.h>  //ADAfruit lib for motor control
+#include <Encoder.h>
+
+AF_DCMotor motor(1, MOTOR12_64KHZ); //registers motor pin and sets the pwm freq
+Encoder myEnc(2,14);
+
+// Define constants
+#define SSSpeed       255   // Motor steady-state speed (0 to 255 as a PWM duty cycle)
+#define SSStart       0    // Time to start of steady-state (seconds)
+#define SSEnd         5    // Time to end of steady-state (seconds)
+#define rampEnd       10    // Time to end of run
+
+// Define variables
+elapsedMillis timer0; //millisecond timer
+long interval = 0; //use long so that interval can be larger than 32767 (in milliseconds)
+double duty = 0;
+// ========================================================================================================================
+
+void setup()
+{
+  // initiaize serial for debug
+  Serial.begin(9600);
+  
+}
+
+// ========================================================================================================================
+
+// Increase pwm duty cycle based on length of time
+void up(int targetSpeed, int targetStart, int dir)
+{
+  timer0 = 0;
+  interval = (targetStart) * 1000L; //need to put L so that interval becomes a long variable
+
+  while (timer0 < interval)
+  {
+    duty = (targetSpeed / 2) * (cos((((timer0) * PI)/interval)+PI) + 1);  //S-shape equation
+    motor.setSpeed(duty); //sets pwm pulse based on equation
+    motor.run(dir); //sets direction of motor
+
+    //debug messages
+    Serial.print(duty);
+    Serial.print("\t");
+    Serial.println("UP");
+  }
+}
+// ========================================================================================================================
+// Decrease pwm duty cycle based on length of time
+void down(int targetStart, int targetEnd, int dir)
+{
+  timer0 = 0;
+  interval = (targetEnd - targetStart) * 1000L;
+
+  while (timer0 < interval)
+  {
+    duty = (SSSpeed / 2) * (cos(((PI / (interval)) * ((targetStart * 1000) + timer0)) - ((PI * targetStart * 1000) / (interval))) + 1);
+    motor.setSpeed(duty);
+    motor.run(dir);
+
+    //debug messages
+    Serial.print(duty);
+    Serial.print("\t");
+    Serial.print(timer0);
+    Serial.print("\t");
+    Serial.print(interval);
+    Serial.print("\t");
+    Serial.println("DOWN");
+
+  }
+}
+// ========================================================================================================================
+// Hold pwm at target value for given time
+void hold(int targetStart, int targetEnd, int dir)
+{
+  timer0 = 0;
+  interval = (targetEnd - targetStart) * 1000L;
+  while (timer0 < interval) {
+    motor.setSpeed(SSSpeed);
+    motor.run(dir);
+    
+    //debug messages
+    //Serial.println(timer0);
+    Serial.print("dir");
+    Serial.print("\t");
+    Serial.println("HOLD");
+  }
+}
+// ========================================================================================================================
+// Hold pwm at target value for given time
+void hold2(int targetStart, int targetEnd, int dir)
+{
+  //timer0 = 0;
+  interval = (targetEnd - targetStart) * 1000L;
+  motor.setSpeed(SSSpeed);
+  motor.run(dir);
+  delay(interval);
+    
+    //debug messages
+    //Serial.println(timer0);
+    Serial.print("dir");
+    Serial.print("\t");
+    Serial.print("HOLD");
+    Serial.print("\t");
+    Serial.println(myEnc.read());
+  }
+
+// ========================================================================================================================
+
+void loop()
+{
+  Serial.println("Waiting..");
+  delay(1000);
+  //Ramp UP
+  //up(SSSpeed, SSStart, FORWARD);
+  //Hold
+  hold2(SSStart, SSEnd, FORWARD);
+  //Ramp DOWN
+  //down(SSEnd, rampEnd, FORWARD);
+  delay(1000);
+  
+  //Ramp UP
+  //up(SSSpeed, SSStart, BACKWARD);
+  //Hold
+  hold2(SSStart, SSEnd, BACKWARD);
+  //Ramp DOWN
+  //down(SSEnd, rampEnd, BACKWARD);
+  //delay(1000);
+  
+}
